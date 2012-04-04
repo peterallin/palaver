7b4ba3d2de3325efbb8a2f67c150cf01497e23eb
Peter Allin <peter@peca.dk>
2012-04-04 13:49:52 +0200

    Example of creating a checklist

---
+++ examples/checklist.rb
@@ -0,0 +1,11 @@
+require 'conversation'
+
+dialog_factory = Conversation::DialogFactory.new
+chosen = dialog_factory.checklist {
+  text "Which do you want?"
+  option "fisk", "A confusing choice for a metasyntactic variable name"
+  option "foo", "The classic first metasyntactic variable name", :on
+  option "bar", "The classic second metasyntactic variable name"
+  option "baz", "The classic third metasyntactic variable name"
+  option "quux", "A quite unusual metasyntactic variable name"
+}
