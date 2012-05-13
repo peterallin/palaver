66aeb3b89dea6e7a4fa097e05f027e6c91755f0b
Peter Allin <peter@peca.dk>
2012-05-13 23:04:41 +0200

    Made the fselect example less demanding on the terminal size.

--- examples/fselect.rb
+++ examples/fselect.rb
@@ -3,10 +3,10 @@ require 'conversation'
 dialog_factory = Conversation::DialogFactory.new
 
 # Using the hash based API for requesting the user to choose a fileectory
-file_hash = dialog_factory.fselect(:path => "/", :width => 60, :height => 30).show
+file_hash = dialog_factory.fselect(:path => "/", :width => 60, :height => 10).show
 
 # Using the block based API for requesting the user to choose a fileectory
-file_block = dialog_factory.fselect { path "/"; width 60; height 30; }.show
+file_block = dialog_factory.fselect { path "/"; width 60; height 10; }.show
 
 puts "File chosen with hash method #{file_hash}"
 puts "File chosen with block method #{file_block}"
