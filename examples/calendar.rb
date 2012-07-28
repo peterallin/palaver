637f04a587adc5df811ede86a9b6200d99e42797
Peter Allin <peter@peca.dk>
2012-07-28 23:11:46 +0200

    Repaired a comment

--- examples/calendar.rb
+++ examples/calendar.rb
@@ -6,7 +6,7 @@ dialog_factory = Palaver::DialogFactory.new(:begin => [2,2], :backtitle => "Defa
 # Using the hash based API with the calendar dialog
 start_date = dialog_factory.calendar(:text => "Choose the start date", :date => Date.new(1939,9,1)).show
 
-# Using the block based API with the calendar dialog. This probably doesn'palaver'
+# Using the block based API with the calendar dialog. This probably doesn't make sense for a 
 # simple widget like the calendar.
 end_date = dialog_factory.calendar { text "Choose the end date"; date Date.new(1945,9,2); }.show
 
